4ec30edf37eddf8fa1ed46b15bd4a96bc3fb406f
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-01-16 21:16:45 +0000

    fixed mongodb connection mixup

--- govgameGameServer/Managers/MongoDBManager.cs
+++ govgameGameServer/Managers/MongoDBManager.cs
@@ -8,9 +8,9 @@ namespace govgameGameServer.Managers
         static readonly string username = "ASPNETwebapp";
         static readonly string password = "deUM3YhG9HreNCQN";
 #if DEBUG
-        static readonly MongoClient mongoClient = new MongoClient($"mongodb://{username}:{password}@localhost");
-#else
         static readonly MongoClient mongoClient = new MongoClient($"mongodb://{username}:{password}@ec2-35-178-4-240.eu-west-2.compute.amazonaws.com");
+#else
+        static readonly MongoClient mongoClient = new MongoClient($"mongodb://{username}:{password}@localhost");
 #endif
 
         #region Notification Database

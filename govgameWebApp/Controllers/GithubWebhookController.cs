093e69838631b1d452277c27cde0d3196811fc51
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-03-29 16:28:36 +0100

    added github webhook controller

---
+++ govgameWebApp/Controllers/GithubWebhookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace govgameWebApp.Controllers
+{
+    public class GithubWebhookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("received github webhook successfully!");
+        }
+    }
+}

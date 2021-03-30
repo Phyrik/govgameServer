208ec147a8efb26905a9c39edf67b10c06caf616
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-03-30 15:31:36 +0100

    test

--- govgameWebApp/Controllers/GithubWebhookController.cs
+++ govgameWebApp/Controllers/GithubWebhookController.cs
@@ -21,7 +21,7 @@ namespace govgameWebApp.Controllers
                 ProcessStartInfo processStartInfo = new ProcessStartInfo()
                 {
                     FileName = "powershell.exe",
-                    Arguments = $"-NoProfile -ExecutionPolicy unrestricted -file \"C:\\Users\\Administrator\\Documents\\deploy.ps1\"",
+                    Arguments = $"-NoProfile -ExecutionPolicy unrestricted -File \"C:\\Users\\Administrator\\Documents\\deploy.ps1\"",
                     UseShellExecute = false
                 };
 

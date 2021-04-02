2591b2e9fd5afe4816f4cc9565592367208d6162
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-04-02 18:47:35 +0100

    better way to return ministry dashboard views

---
+++ govgameSharedClasses/Helpers/ViewHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace govgameSharedClasses.Helpers
+{
+    public class ViewHelper
+    {
+        public static ViewResult MinistryDashboardView(Controller controller, string rootPath, MinistryHelper.MinistryCode ministryCode, string page)
+        {
+            string ministryFolderName = "";
+            switch (ministryCode)
+            {
+                case MinistryHelper.MinistryCode.None:
+                    return null;
+
+                case MinistryHelper.MinistryCode.PrimeMinister:
+                    ministryFolderName = "PrimeMinisterDashboard";
+                    break;
+
+                case MinistryHelper.MinistryCode.Interior:
+                    break;
+
+                case MinistryHelper.MinistryCode.FinanceAndTrade:
+                    ministryFolderName = "FinanceAndTradeDashboard";
+                    break;
+
+                case MinistryHelper.MinistryCode.ForeignAffairs:
+                    break;
+
+                case MinistryHelper.MinistryCode.Defence:
+                    break;
+            }
+
+            if (File.Exists($@"{rootPath}\Views\Game\{ministryFolderName}\{page}.cshtml"))
+            {
+                return controller.View($"./{ministryFolderName}/{page}");
+            }
+            else
+            {
+                return controller.View("404");
+            }
+        }
+    }
+}

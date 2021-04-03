25e592681b40be948de0aa49a10788f7bbb6eef3
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-04-03 17:19:48 +0100

    made powershell script better

--- mergeMasterIntoFeatures.ps1
+++ mergeMasterIntoFeatures.ps1
@@ -1,8 +1,28 @@
+$fromBranch = Read-Host "Which branch do you want to merge into master? (<branchname>/none/current)"
+
 $currentBranch = git rev-parse --abbrev-ref HEAD
 $allBranches = "primeminister","financeandtrade"
+
+switch ($fromBranch) {
+	"none" {
+		Break
+	}
+	"current" {
+		git checkout master
+		git merge $currentBranch
+		git push
+	}
+	Default {
+		git checkout master
+		git merge $fromBranch
+		git push
+	}
+}
+
 foreach ($branch in $allBranches) {
 	git checkout $branch
 	git merge master
 	git push
 }
+
 git checkout $currentBranch

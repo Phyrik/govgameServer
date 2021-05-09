1dbb223dc5bff7b8247fdc99294d3138919af36c
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-05-09 22:02:41 +0100

    make country name a primary key

--- govgameSharedClasses/Models/MySQL/Country.cs
+++ govgameSharedClasses/Models/MySQL/Country.cs
@@ -1,7 +1,10 @@
-﻿namespace govgameSharedClasses.Models.MySQL
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace govgameSharedClasses.Models.MySQL
 {
     public class Country
     {
+        [Key]
         public string CountryName { get; set; }
         public string CapitalName { get; set; }
         public int FlagId { get; set; }

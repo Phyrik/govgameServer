4364e0061dd523513a6629d7818669812bf396c7
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-05-13 19:28:50 +0100

    add invite minister typescript

---
+++ govgameWebApp/wwwroot/js/Game/Invite/Minister.js
@@ -0,0 +1,44 @@
+function accept(ministerToReplacePM) {
+    if (isPrimeMinister && !noMinistersToReplace) {
+        if (ministerToReplacePM === null) {
+            document.getElementById("pm-ministry-choice-div").style.display = "";
+            return;
+        }
+        document.getElementById("please-wait-h4").style.display = "";
+        $.post("/Game/AcceptMinistryInvite?ministry=" + ministryCode + "&newCountryName=" + newCountryName + "&ministryToReplacePM=" + ministerToReplacePM, function (data, status) {
+            if (data == "success") {
+                alert("Successfully accepted invite");
+                location.href = "/Game/CountryDashboard";
+            }
+            else {
+                alert(data);
+            }
+        });
+        return;
+    }
+    if (noMinistersToReplace) {
+        if (!confirm("Are you sure you want to leave your old country? It will be deleted if you choose to do so.")) {
+            return;
+        }
+    }
+    document.getElementById("please-wait-h4").style.display = "";
+    $.post("/Game/AcceptMinistryInvite?ministry=" + ministryCode + "&newCountryName=" + newCountryName, function (data, status) {
+        if (data == "success") {
+            alert("Successfully accepted invite");
+            location.href = "/Game/CountryDashboard";
+        }
+    });
+}
+function decline() {
+    document.getElementById("please-wait-h4").style.display = "";
+    $.post("/Game/DeclineMinistryInvite?ministry=" + ministryCode + "&newCountryName=" + newCountryName, function (data, status) {
+        if (data == "success") {
+            alert("Successfully declined invite");
+            location.href = "/Game/CountryDashboard";
+        }
+        else {
+            alert(data);
+        }
+    });
+}
+//# sourceMappingURL=Minister.js.map

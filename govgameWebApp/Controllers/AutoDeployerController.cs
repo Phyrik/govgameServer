d7934ae6f3ad18045bde29aab738254610572fe1
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-05-13 18:38:33 +0100

    fix path for auto deploys

--- govgameWebApp/Controllers/AutoDeployerController.cs
+++ govgameWebApp/Controllers/AutoDeployerController.cs
@@ -15,7 +15,7 @@ namespace govgameWebApp.Controllers
             if (requestBodyJObject["ref"].ToString().Split('/')[2] == "master")
             {
                 Console.WriteLine($"Pushing with commit {requestBodyJObject["before"]}...");
-                ProcessStartInfo processStartInfo = new ProcessStartInfo { FileName = "/bin/bash", Arguments = "/home/pi/Documents/govgameServer/deploy.sh", UseShellExecute = true };
+                ProcessStartInfo processStartInfo = new ProcessStartInfo { FileName = "/bin/bash", Arguments = "/home/pi/govgameServer/deploy.sh", UseShellExecute = true };
                 Process process = new Process { StartInfo = processStartInfo };
                 process.Start();
                 Environment.Exit(0);

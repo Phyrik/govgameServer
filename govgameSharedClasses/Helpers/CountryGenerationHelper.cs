79745e8c17aa316e00d3344aeec0af03b64a21dc
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-05-13 17:19:01 +0100

    add italy flag

--- govgameSharedClasses/Helpers/CountryGenerationHelper.cs
+++ govgameSharedClasses/Helpers/CountryGenerationHelper.cs
@@ -5,7 +5,7 @@ namespace govgameSharedClasses.Helpers
 {
     public class CountryGenerationHelper
     {
-        public static readonly string[] flagNames = new string[] { "default", "france", "germany", "india", "russia", "uk", "usa" };
+        public static readonly string[] flagNames = new string[] { "default", "france", "germany", "india", "russia", "uk", "usa", "italy" };
 
         public static int FlagNameToId(string flagName)
         {

7c841e7d3734b6ea300069533b6ff2f310407927
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-05-13 20:01:11 +0100

    add invite new minister typescript

---
+++ govgameWebApp/scripts/Game/PrimeMinisterDashboard/InviteNewMinister.ts
@@ -0,0 +1,39 @@
+﻿declare var ministryCode: string;
+
+function inviteMinister(username) {
+    $.post(`/Game/InviteMinister?ministry=${ministryCode}&invitedUsername=${username}`, function (data, status) {
+        if (data == "success") {
+            location.href = `/Game/PrimeMinisterDashboard/ViewMinistry?ministry=${ministryCode}`;
+        }
+        else {
+            alert(data);
+        }
+    })
+}
+
+function searchUsers() {
+    var searchText = $("#search-input").val() as string;
+
+    var lis = $("#user-ul")[0].getElementsByTagName("li");
+    var hrs = $("#user-ul")[0].getElementsByTagName("hr");
+
+    for (var i = 0; i < lis.length; i++) {
+        var name = lis[i].getElementsByTagName("h2")[0].innerHTML;
+
+        if (name.toLowerCase().indexOf(searchText.toLowerCase()) !== -1) {
+            lis[i].style.display = "grid";
+            hrs[i].style.display = "";
+        }
+        else {
+            lis[i].style.display = "none";
+            hrs[i].style.display = "none";
+        }
+    }
+}
+
+document.getElementById("search-input").addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        searchUsers();
+    }
+})

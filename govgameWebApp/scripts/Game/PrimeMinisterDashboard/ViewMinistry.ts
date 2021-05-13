1189b8a259cb40817f5b2868fcf1680652d6fbb3
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-05-13 20:11:01 +0100

    add view ministry typescript

---
+++ govgameWebApp/scripts/Game/PrimeMinisterDashboard/ViewMinistry.ts
@@ -0,0 +1,34 @@
+﻿declare var ministryCode: string;
+
+var ministerHeading = document.getElementById("minister")
+var ministerButton = document.getElementById("minister-button")
+
+function dismissMinister() {
+    var okPressed = confirm("Are you sure you want to dismiss this minister?")
+
+    if (!okPressed) return
+
+    ministerHeading.innerHTML = "No minister"
+    ministerButton.onclick = function () { location.href = `/Game/PrimeMinisterDashboard/InviteNewMinister?minister=${ministryCode}` }
+    ministerButton.innerHTML = "Invite new minister"
+
+    $.post(`/Game/DismissMinister?minister=${ministryCode}`, function (data, status) {
+        if (data != "success") {
+            alert(data)
+            location.reload()
+        }
+    })
+}
+
+function cancelMinistryInvite() {
+    ministerHeading.innerHTML = "No minister"
+    ministerButton.onclick = function () { location.href = `/Game/PrimeMinisterDashboard/InviteNewMinister?minister=${ministryCode}` }
+    ministerButton.innerHTML = "Invite new minister"
+
+    $.post(`/Game/CancelMinistryInvite?ministry=${ministryCode}`, function (data, status) {
+        if (data != "success") {
+            alert(data)
+            location.reload()
+        }
+    })
+}

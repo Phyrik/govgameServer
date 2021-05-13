8e9cb93a298585207742577be3ecf0bceab2bef6
Phyrik <34688504+Phyrik@users.noreply.github.com>
2021-05-13 19:16:10 +0100

    notifications typescript added

---
+++ govgameWebApp/scripts/Game/Notifications.ts
@@ -0,0 +1,16 @@
+﻿declare var readNotOpenedColour: string;
+declare var unreadNotOpenedColour: string;
+
+function markNotificationAsRead(notificationId) {
+    var notificationLi = document.getElementById("notification-li-" + notificationId);
+
+    notificationLi.style.backgroundColor = readNotOpenedColour;
+
+    $.post(`/Game/MarkNotificationAsRead?notificationId=${notificationId}`, function (data, status) {
+        if (data != "success") {
+            console.log("error setting notification as read");
+            alert(data);
+            notificationLi.style.backgroundColor = unreadNotOpenedColour;
+        }
+    })
+}
---
+++ govgameWebApp/wwwroot/js/Game/Notifications.js
@@ -0,0 +1,12 @@
+function markNotificationAsRead(notificationId) {
+    var notificationLi = document.getElementById("notification-li-" + notificationId);
+    notificationLi.style.backgroundColor = readNotOpenedColour;
+    $.post("/Game/MarkNotificationAsRead?notificationId=" + notificationId, function (data, status) {
+        if (data != "success") {
+            console.log("error setting notification as read");
+            alert(data);
+            notificationLi.style.backgroundColor = unreadNotOpenedColour;
+        }
+    });
+}
+//# sourceMappingURL=Notifications.js.map
